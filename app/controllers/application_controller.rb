2a63d419b4d0d02dc5473605547d72173a18ce49
palette3995 <sasadangon@icloud.com>
2023-01-02 18:43:15 +0900

    [style] #17 %i記法の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i(name part))
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name part])
   end
 end
