7c658cfe6e39dfd828df44981a4f639da0fc950d
palette3995 <sasadangon@icloud.com>
2023-01-14 18:48:40 +0900

    [add] #UserPartモデルにsetupメソッド作成
    新規登録時に自動的にUserPartモデルのインスタンスを作成し、editで編集可能にする為

--- app/models/user_part.rb
+++ app/models/user_part.rb
@@ -1,4 +1,11 @@
 class UserPart < ApplicationRecord
   belongs_to :user
   belongs_to :part
+
+  def self.setup(user)
+    3.times do |n|
+      user.user_parts.create(user_id: user.id, part_id: 7, priority: n + 1)
+    end
+
+  end
 end
