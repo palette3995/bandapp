8bf8edb4e270c3364d85b6784cfb66cce77151bc
palette3995 <sasadangon@icloud.com>
2023-01-22 15:50:14 +0900

    [add] #20 曜日と時間帯を取得するメソッドを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,12 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name])
   end
+
+  def get_week_day
+    @week_days = ["日","月","火","水","木","金","土"]
+  end
+
+  def get_activity_time
+    @activity_times = ["朝","昼","夜"]
+  end
 end
