f23f1905e0b07d291003f4988d95f8be7b9a905a
palette3995 <sasadangon@icloud.com>
2023-01-22 15:52:05 +0900

    [add] #20 編集画面のビューとコントローラーを作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
+  before_action :get_week_day
+  before_action :get_activity_time
+  before_action :get_level
   def index
+    @user = User.find(current_user.id)
+    UserPart.setup(@user) if @user.user_parts.blank?
   end
 
   def show
@@ -10,9 +15,52 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(current_user.id)
+    @user = User.find(params[:id])
+    @parts = Part.all
+    @user_parts = @user.parts
+
   end
 
   def update
+    @user = User.find(params[:id])
+    @parts = Part.all
+    @user_parts = @user.parts
+    @user.update(user_params)
+    redirect_to mypage_path
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :name,
+      :region,
+      :age,
+      :sex,
+      :genre,
+      :favorite,
+      :introduction,
+      :image,
+      :movie,
+      :sound,
+      :original,
+      :want_to_copy,
+      :motivation,
+      :other_part,
+      :frequency,
+      :activity_time,
+      :vailable_day,
+      :compose,
+      user_parts_attributes: %i[id part_id user_id level _destroy]
+    )
+  end
+
+  def get_level
+    @levels = ["未経験","初心者","中級者","上級者"]
+  end
+
+  def delete_media(media)
+    media.purge
+  end
+
 end
