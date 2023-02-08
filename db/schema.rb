737a76b81b618f2b13a540b80f6f21b748edf358
palette3995 <sasadangon@icloud.com>
2023-02-08 16:05:47 +0900

    [change] #20 データ型をintegerへ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_08_064141) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_08_070433) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -54,7 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_08_064141) do
   create_table "user_genres", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "genre_id"
-    t.string "priority"
+    t.integer "priority"
     t.string "other_genre"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
