37b96e4e581b1199d78dcd273d57f15328b704c9
palette3995 <sasadangon@icloud.com>
2023-02-12 14:37:03 +0900

    [refactor] #20 after_createメソッドのuser_id指定の記法を修正。

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,8 +20,8 @@ class User < ApplicationRecord
 
   after_create do
     3.times do |n|
-      self.user_parts.create(user_id: self.id, part_id: 7, priority: n + 1)
-      self.user_genres.create(user_id: self.id, genre_id: 16, priority: n + 1)
+      user_parts.create(user_id: id, part_id: 7, priority: n + 1)
+      user_genres.create(user_id: id, genre_id: 16, priority: n + 1)
     end
   end
 end
