24229efc008bf20c69c421cb8945dcf07206aed3
palette3995 <sasadangon@icloud.com>
2023-02-16 18:04:46 +0900

    [add] #28 ユーザーの初期データ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,3 +32,54 @@
 # Genre.create!(id: 14, name: "クラシック")
 # Genre.create!(id: 15, name: "その他")
 # Genre.create!(id: 16, name: "未選択")
+
+# 30.times do |n|
+#   User.create!(
+#     name: "テストユーザー(大阪)#{n+1}",
+#     email: "#{n+1}@#{n+1}",
+#     password: 111111,
+#     prefecture_id: 27,
+#     introduction: "よろしくお願いします！",
+#     age: n + 18,
+#     sex: "男性",
+#     original: "オリジナル曲",
+#     frequency: "月２〜３日",
+#     motivation: "趣味で楽しく",
+#     activity_time: "夜",
+#     available_day: "日,土"
+#   )
+# end
+#
+# 30.times do |n|
+#   User.create!(
+#     name: "テストユーザー(東京)#{n+1}",
+#     email: "#{n+31}@#{n+31}",
+#     password: 111111,
+#     prefecture_id: 13,
+#     introduction: "よろしくお願いします！",
+#     age: n + 18,
+#     sex: "男性",
+#     original: "オリジナル曲",
+#     frequency: "月２〜３日",
+#     motivation: "趣味で楽しく",
+#     activity_time: "夜",
+#     available_day: "日,土"
+#   )
+# end
+#
+# 30.times do |n|
+#   User.create!(
+#     name: "テストユーザー(京都)#{n+1}",
+#     email: "#{n+61}@#{n+61}",
+#     password: 111111,
+#     prefecture_id: 26,
+#     introduction: "よろしくお願いします！",
+#     age: n + 18,
+#     sex: "男性",
+#     original: "オリジナル曲",
+#     frequency: "月２〜３日",
+#     motivation: "趣味で楽しく",
+#     activity_time: "夜",
+#     available_day: "日,土"
+#   )
+# end
