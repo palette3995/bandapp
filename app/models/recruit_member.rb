ab32677b00279f7df95ecf058c24e6598874a944
palette3995 <sasadangon@icloud.com>
2023-02-21 21:01:26 +0900

    [add] #30 Favoriteモデルとのアソシエーションを設定

--- app/models/recruit_member.rb
+++ app/models/recruit_member.rb
@@ -1,4 +1,7 @@
 class RecruitMember < ApplicationRecord
   belongs_to :band
   belongs_to :part
+  # Favoriteモデルとのアソシエーション
+  has_many :reverse_of_favorites, class_name: "Favorite", dependent: :destroy
+  has_many :favorited_mes, through: :reverse_of_favorites, source: :user
 end
