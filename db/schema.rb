fa454c3da74cd1428922be2cbf4f3693dba7ea2c
palette3995 <sasadangon@icloud.com>
2023-03-01 13:19:24 +0900

    [add] #32 band_membersテーブルにother_partカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_01_023710) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_01_034513) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -57,6 +57,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_01_023710) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "part_id"
+    t.string "other_part"
     t.index ["band_id"], name: "index_band_members_on_band_id"
     t.index ["part_id"], name: "index_band_members_on_part_id"
     t.index ["user_id"], name: "index_band_members_on_user_id"
@@ -128,6 +129,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_01_023710) do
     t.string "scouted_other_part"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "message"
     t.index ["band_id"], name: "index_scouts_on_band_id"
     t.index ["part_id"], name: "index_scouts_on_part_id"
     t.index ["scouted_band_id"], name: "index_scouts_on_scouted_band_id"
