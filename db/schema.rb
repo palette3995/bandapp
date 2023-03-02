ce0a6e399e4451bc274da95a4013d8fa4c38b885
palette3995 <sasadangon@icloud.com>
2023-03-02 13:22:16 +0900

    [add] #32 frequencyカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_01_034513) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_02_041459) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -75,6 +75,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_01_034513) do
     t.string "avairable_day"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "frequency"
   end
 
   create_table "favorites", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
