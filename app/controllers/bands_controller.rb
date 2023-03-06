591dbb1af53033a917cb833157d408c4ea043077
palette3995 <sasadangon@icloud.com>
2023-03-06 19:00:45 +0900

    [add] #32 show,edit,updateアクションの実装

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -6,18 +6,52 @@ class BandsController < ApplicationController
 
   def show
     @band = Band.find(params[:id])
+    @members = @band.band_members
+    @recruits = @band.recruit_members
+    @reader = @members.find_by(role: "リーダー").user
+    @delete_id = @members.find_by(user_id: current_user.id).id
   end
 
   def edit
     @band = Band.find(params[:id])
-  end
-
-  def create
+    @genres = Genre.all
   end
 
   def update
+    @band = Band.find(params[:id])
+    # メディア削除ボタンが押された際の処理
+    if params[:delete_image]
+      delete_media(@band.image)
+    else
+      # 登録完了後の処理
+      @band.update(user_params)
+      redirect_to band_path
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def band_params
+    params.require(:band).permit(
+      :name,
+      :prefecture_id,
+      :introduction,
+      :image,
+      :original,
+      :want_to_copy,
+      :motivation,
+      :frequency,
+      :activity_time,
+      :available_day,
+      band_genres_attributes: %i[id genre_id user_id other_genre _destroy]
+    )
+  end
+
+  def delete_media(media)
+    media.purge
+    render action: "edit"
   end
 end
