84cdf44663542c8d1d3169617ccc4e5a314c933a
palette3995 <sasadangon@icloud.com>
2023-03-14 22:39:34 +0900

    [add] #38 create後にユーザーへ通知を送る機能を実装

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,6 +1,20 @@
 class Favorite < ApplicationRecord
+  after_create_commit :create_notifications
   belongs_to :user
   belongs_to :favorited_user, class_name: "User", optional: true
   belongs_to :band, optional: true
   has_one :notification, as: :subject, dependent: :destroy
+
+  private
+
+  def create_notifications
+    if favorited_user_id.present?
+      Notification.create!(subject: self, user_id: favorited_user_id, action_type: Notification.notification_types[:favorite_you])
+    else
+      band = Band.find(band_id)
+      band.band_members.each do |member|
+        Notification.create!(subject: self, user_id: member.user_id, action_type: Notification.notification_types[:favorite_your_band])
+      end
+    end
+  end
 end
--- app/models/scout.rb
+++ app/models/scout.rb
@@ -1,4 +1,5 @@
 class Scout < ApplicationRecord
+  after_create_commit :create_notifications
   validates :message, length: { maximum: 30 }
   belongs_to :user
   belongs_to :scouted_user, class_name: "User", optional: true
@@ -7,4 +8,18 @@ class Scout < ApplicationRecord
   belongs_to :part, optional: true
   belongs_to :scouted_part, class_name: "Part", optional: true
   has_one :notification, as: :subject, dependent: :destroy
+
+  private
+
+  def create_notifications
+    if band_id.nil? && scouted_band_id.nil?
+      Notification.create!(subject: self, user_id: favorited_user_id, action_type: Notification.notification_types[:new_band_scout])
+    elsif band_id.present? && scouted_band_id.nil?
+      Notification.create!(subject: self, user_id: favorited_user_id, action_type: Notification.notification_types[:band_scout_you])
+    elsif band_id.nil? && scouted_band_id.present?
+      Notification.create!(subject: self, user_id: favorited_user_id, action_type: Notification.notification_types[:want_to_join_your_band])
+    elsif band_id.present? && scouted_band_id.present?
+      Notification.create!(subject: self, user_id: favorited_user_id, action_type: Notification.notification_types[:marge_band_request])
+    end
+  end
 end
