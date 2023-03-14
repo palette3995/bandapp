24feedde0af16b0926388154eb29529c8c462ab5
palette3995 <sasadangon@icloud.com>
2023-03-14 22:38:03 +0900

    [add] #38 notification_typeの値をenumで割り振り

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,3 +1,12 @@
 class Notification < ApplicationRecord
   belongs_to :subject, polymorphic: true
+
+  enum notification_type: {
+    favorite_you: 0,
+    favorite_your_band: 1,
+    new_band_scout: 2,
+    band_scout_you: 3,
+    want_to_join_your_band: 4,
+    marge_band_request: 5
+  }
 end
