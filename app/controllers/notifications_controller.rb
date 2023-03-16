788d365bb2df22c594250ef79fa56cae5ce17914
palette3995 <sasadangon@icloud.com>
2023-03-16 15:10:24 +0900

    [add] #38 notificationsコントローラー作成

---
+++ app/controllers/notifications_controller.rb
@@ -0,0 +1,26 @@
+class NotificationsController < ApplicationController
+  def index
+    @notifications = current_user.notifications.order(created_at: :desc).page(params[:page])
+  end
+
+  def unreads
+    @notifications = current_user.notifications.where(read: false).order(created_at: :desc).page(params[:page])
+  end
+
+  def read
+    notification = current_user.notifications.find(params[:id])
+    unless notification.read?
+      notification.update(read: true)
+    end
+    redirect_to notification.subject.redirect_path
+  end
+
+  def read_all
+    notifications = current_user.notifications.where(read: false)
+    notifications.each do |notification|
+      notification.update(read: true)
+    end
+    redirect_to notifications_path
+  end
+
+end
