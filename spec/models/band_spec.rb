23cdac87d96fbcd0e6c227c0c9727dd3bf7ce7ea
palette3995 <sasadangon@icloud.com>
2023-03-20 19:42:32 +0900

    [add] #48 Band model spec作成

---
+++ spec/models/band_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe Band do
+  let!(:band) { create(:band) }
+
+  it "名前を入力していないとき登録できないこと" do
+    band.name = nil
+    expect(band).not_to be_valid
+    expect(band.errors).not_to be_empty
+  end
+
+  it "その他のジャンル名が15文字以上であれば登録できないこと" do
+    genre = create(:other_music)
+    band_genre = create(:band_genre, band: band, genre: genre)
+    band_genre.other_genre = "test_other_genre"
+    expect(band_genre).not_to be_valid
+    expect(band_genre.errors).not_to be_empty
+  end
+end
