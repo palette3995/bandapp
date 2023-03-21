a467821b07b1754884918ea43961d9f0bbacdd8f
palette3995 <sasadangon@icloud.com>
2023-03-21 13:49:33 +0900

    [add] #48 登録できるパターンのテストを追加

--- spec/models/band_spec.rb
+++ spec/models/band_spec.rb
@@ -1,7 +1,13 @@
 require "rails_helper"
 
 RSpec.describe Band do
+  let!(:genre) {create(:other_music)}
   let!(:band) { create(:band) }
+  let!(:band_genre) { create(:band_genre, band: band, genre: genre, other_genre: "other_genre") }
+
+  it "名前が入力されており、その他のジャンル名が15文字以内であれば登録できること" do
+    expect(band).to be_valid
+  end
 
   it "名前を入力していないとき登録できないこと" do
     band.name = nil
@@ -10,8 +16,6 @@ RSpec.describe Band do
   end
 
   it "その他のジャンル名が16文字以上であれば登録できないこと" do
-    genre = create(:other_music)
-    band_genre = create(:band_genre, band: band, genre: genre)
     band_genre.other_genre = "test_other_genre"
     expect(band_genre).not_to be_valid
     expect(band_genre.errors).not_to be_empty
