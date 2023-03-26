10cdc087248bda052956fb6092985b1417d168f8
palette3995 <sasadangon@icloud.com>
2023-03-26 14:38:17 +0900

    [change] ログイン後のurlを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
-    users_path(resource)
+    users_path
   end
 
   protected
