09c7d3121a2d8f67f663590b28ffb7243f0c99f1
palette3995 <sasadangon@icloud.com>
2023-03-28 16:11:53 +0900

    [refactor] 記法の修正、簡略化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(_resource)
     users_path
   end
 
@@ -12,8 +12,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name])
   end
 
-  def update_band_colums(id)
-    band = Band.find(id)
+  def update_band_colums(band)
     band.update!(
       number_of_member: band.band_members.count,
       maximum_age: band.band_members.joins(:user).maximum(:age),
