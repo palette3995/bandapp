b359d6d16718cbed1f816e63da13fab740e652f2
palette3995 <95852095+palette3995@users.noreply.github.com>
2023-03-29 15:57:32 +0900

    [change] #50 ログインの有無によるリダイレクトのテストを1アクションに限定
    コード記述量削減の為

--- spec/requests/bands_spec.rb
+++ spec/requests/bands_spec.rb
@@ -30,156 +30,102 @@ RSpec.describe "Bands" do
   end
 
   describe "GET bands#match_ages" do
-    context "ユーザーがログインしているとき" do
-      before do
-        sign_in user
-        get match_ages_bands_path
-      end
-
-      it "リクエストが200 OKとなること" do
-        expect(response).to have_http_status :ok
-      end
-
-      it "タイトルが正しく表示されること" do
-        expect(response.body).to include("平均年齢が近いバンド")
-      end
+    before do
+      sign_in user
+      get match_ages_bands_path
     end
 
-    context "ユーザーがログインしていないとき" do
-      it "ログインページに遷移すること" do
-        get match_ages_bands_path
-        expect(response).to have_http_status :found
-      end
+    it "リクエストが200 OKとなること" do
+      expect(response).to have_http_status :ok
+    end
+
+    it "タイトルが正しく表示されること" do
+      expect(response.body).to include("平均年齢が近いバンド")
     end
   end
 
   describe "GET bands#match_genres" do
-    context "ユーザーがログインしているとき" do
-      before do
-        sign_in user
-        get match_genres_bands_path
-      end
-
-      it "リクエストが200 OKとなること" do
-        expect(response).to have_http_status :ok
-      end
-
-      it "タイトルが正しく表示されること" do
-        expect(response.body).to include("ジャンルが同じなバンド")
-      end
+    before do
+      sign_in user
+      get match_genres_bands_path
     end
 
-    context "ユーザーがログインしていないとき" do
-      it "ログインページに遷移すること" do
-        get match_genres_users_path
-        expect(response).to have_http_status :found
-      end
+    it "リクエストが200 OKとなること" do
+      expect(response).to have_http_status :ok
+    end
+
+    it "タイトルが正しく表示されること" do
+      expect(response.body).to include("ジャンルが同じなバンド")
     end
   end
 
   describe "GET bands#show" do
-    context "ユーザーがログインしているとき" do
-      before do
-        band_member
-        sign_in user
-        get band_path(band)
-      end
-
-      it "リクエストが200 OKとなること" do
-        expect(response).to have_http_status :ok
-      end
-
-      it "バンド名が正しく表示されること" do
-        expect(response.body).to include(band.name)
-      end
+    before do
+      band_member
+      sign_in user
+      get band_path(band)
     end
 
-    context "ユーザーがログインしていないとき" do
-      it "ログインページに遷移すること" do
-        get band_path(band)
-        expect(response).to have_http_status :found
-      end
+    it "リクエストが200 OKとなること" do
+      expect(response).to have_http_status :ok
+    end
+
+    it "バンド名が正しく表示されること" do
+      expect(response.body).to include(band.name)
     end
   end
 
   describe "GET bands#edit" do
-    context "ユーザーがログインしているとき" do
-      before do
-        sign_in user
-        band_member
-        get edit_band_path(band)
-      end
-
-      it "リクエストが200 OKとなること" do
-        expect(response).to have_http_status :ok
-      end
-
-      it "バンド名が正しく表示されること" do
-        expect(response.body).to include(band.name)
-      end
-
-      it "自分の所属バンド以外のページにアクセスしたとき、バンド一覧ページに遷移すること" do
-        band_a = create(:band)
-        get edit_band_path(band_a)
-        expect(response).to redirect_to user_bands_bands_path
-      end
+    before do
+      sign_in user
+      band_member
+      get edit_band_path(band)
     end
 
-    context "ユーザーがログインしていないとき" do
-      it "ログインページに遷移すること" do
-        get edit_band_path(band)
-        expect(response).to have_http_status :found
-      end
+    it "リクエストが200 OKとなること" do
+      expect(response).to have_http_status :ok
+    end
+
+    it "バンド名が正しく表示されること" do
+      expect(response.body).to include(band.name)
+    end
+
+    it "自分の所属バンド以外のページにアクセスしたとき、バンド一覧ページに遷移すること" do
+      band_a = create(:band)
+      get edit_band_path(band_a)
+      expect(response).to redirect_to user_bands_bands_path
     end
   end
 
   describe "GET bands#search" do
-    context "ユーザーがログインしているとき" do
-      before do
-        band_member
-        sign_in user
-        get search_bands_path
-      end
-
-      it "リクエストが200 OKとなること" do
-        expect(response).to have_http_status :ok
-      end
-
-      it "タイトルが正しく表示されること" do
-        expect(response.body).to include("検索結果")
-      end
+    before do
+      band_member
+      sign_in user
+      get search_bands_path
     end
 
-    context "ユーザーがログインしていないとき" do
-      it "ログインページに遷移すること" do
-        get search_bands_path
-        expect(response).to have_http_status :found
-      end
+    it "リクエストが200 OKとなること" do
+      expect(response).to have_http_status :ok
+    end
+
+    it "タイトルが正しく表示されること" do
+      expect(response.body).to include("検索結果")
     end
   end
 
   describe "GET bands#user_bands" do
-    context "ユーザーがログインしているとき" do
-      before do
-        band_member
-        sign_in user
-        get user_bands_bands_path
-      end
-
-      it "リクエストが200 OKとなること" do
-        expect(response).to have_http_status :ok
-      end
-
-      it "バンド名が正しく表示されること" do
-        expect(response.body).to include(band.name)
-      end
+    before do
+      band_member
+      sign_in user
+      get user_bands_bands_path
     end
 
-    context "ユーザーがログインしていないとき" do
-      it "ログインページに遷移すること" do
-        get user_bands_bands_path
-        expect(response).to have_http_status :found
-      end
+    it "リクエストが200 OKとなること" do
+      expect(response).to have_http_status :ok
+    end
+
+    it "バンド名が正しく表示されること" do
+      expect(response.body).to include(band.name)
     end
   end
 end
