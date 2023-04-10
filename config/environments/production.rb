57c9be6fdfa12c7f4fe2552e57d54a401a3114ac
palette3995 <sasadangon@icloud.com>
2023-04-10 21:33:42 +0900

    [add] #55 hostを設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,6 +90,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: 'portfolio-ryota-matsumoto.herokuapp.com' }
   config.action_mailer.raise_delivery_errors = true
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
